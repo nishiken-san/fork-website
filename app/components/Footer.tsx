c975d95343f0b03e1e718050351f9fbee543ea91
nishiken-san <108216780+nishiken-san@users.noreply.github.com>
2026-03-08 21:10:13 +0900

    0308fix

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
       <style jsx>{`
         .footer-bg {
           background-color: #003705;
-          padding: 100px 0 20px 0;
+          padding: 100px 0 18px 0;
         }
         
         .footer-container {
